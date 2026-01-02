f1f919a0fad39bac60974bbe4cae7702dc1f89d8
anushkaparmar5 <anushkaparmar512@gmail.com>
2026-01-02 13:32:22 +0530

    commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,41 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [
+//     react({
+//       babel: {
+//         plugins: [['babel-plugin-react-compiler']],
+//       },
+//     }),
+//   ],
+// })
+
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
 export default defineConfig({
-  plugins: [
-    react({
-      babel: {
-        plugins: [['babel-plugin-react-compiler']],
+  plugins: [react()],
+  // Add base URL for deployment
+  base: "./",
+  // Build configuration
+  build: {
+    outDir: "dist",
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ["react", "react-dom", "react-redux"],
+          redux: ["@reduxjs/toolkit", "redux"],
+        },
       },
-    }),
-  ],
-})
+    },
+  },
+  // Server configuration
+  server: {
+    headers: {
+      "Content-Type": "application/javascript",
+    },
+  },
+});
